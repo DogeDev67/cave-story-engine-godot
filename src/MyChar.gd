25d67ce094b7cf0a7e22daa479d80f0023468777
DogeDev67 <alpicavlak@gmail.com>
2024-10-24 17:29:59 +0200

    updated mychar

--- src/MyChar.gd
+++ src/MyChar.gd
@@ -2,24 +2,77 @@ extends Node
 
 func InitMyChar() -> void:
 	MyCharHeader.gMC.cond = 0x80;
-	MyCharHeader.gMC.direct = 2;
-
+	MyCharHeader.gMC
 	MyCharHeader.gMC.view.back = 8 * 0x200;
 	MyCharHeader.gMC.view.top = 8 * 0x200;
 	MyCharHeader.gMC.view.front = 8 * 0x200;
-	MyCharHeader.gMC.view.bottom = 8 * 0x200;
-
+	MyCharHeader.gMC.view.bottom 
 	MyCharHeader.gMC.hit.back = 5 * 0x200;
 	MyCharHeader.gMC.hit.top = 8 * 0x200;
 	MyCharHeader.gMC.hit.front = 5 * 0x200;
-	MyCharHeader.gMC.hit.bottom = 8 * 0x200;
-
+	MyCharHeader.gMC.hit.bottom 
 	MyCharHeader.gMC.life = 3;
 	MyCharHeader.gMC.max_life = 3;
 	MyCharHeader.gMC.unit = 0;
 
-func AnimationMyChar(bKey : bool) -> void:
-    pass
+# Function to handle animation logic
+func animation_my_char(b_key: bool) -> void:
+	# Check if the character is frozen or in special condition
+	if MyCharHeader.gMC["cond"] & 2:
+		return
+
+	# Character is grounded (flag & 8 means grounded)
+	if MyCharHeader.gMC["flag"] & 8:
+		if MyCharHeader.gMC["cond"] & 1:
+			MyCharHeader.gMC["ani_no"] = 11
+		elif Input.is_action_pressed(gKeyUp) and (Input.is_action_pressed(gKeyLeft) or Input.is_action_pressed(gKeyRight)) and b_key:
+			MyCharHeader.gMC["cond"] |= 4
+			MyCharHeader.gMC["ani_wait"] += 1
+			if MyCharHeader.gMC["ani_wait"] > 4:
+				MyCharHeader.gMC["ani_wait"] = 0
+				MyCharHeader.gMC["ani_no"] += 1
+				if MyCharHeader.gMC["ani_no"] in [7, 9]:
+					play_sound(24)
+			
+			if MyCharHeader.gMC["ani_no"] > 9 or MyCharHeader.gMC["ani_no"] < 6:
+				MyCharHeader.gMC["ani_no"] = 6
+		elif (Input.is_action_pressed(gKeyLeft) or Input.is_action_pressed(gKeyRight)) and b_key:
+			MyCharHeader.gMC["cond"] |= 4
+			MyCharHeader.gMC["ani_wait"] += 1
+			if MyCharHeader.gMC["ani_wait"] > 4:
+				MyCharHeader.gMC["ani_wait"] = 0
+				MyCharHeader.gMC["ani_no"] += 1
+				if MyCharHeader.gMC["ani_no"] in [2, 4]:
+					play_sound(24)
+			
+			if MyCharHeader.gMC["ani_no"] > 4 or MyCharHeader.gMC["ani_no"] < 1:
+				MyCharHeader.gMC["ani_no"] = 1
+		elif Input.is_action_pressed(gKeyUp) and b_key:
+			if MyCharHeader.gMC["cond"] & 4:
+				play_sound(24)
+			MyCharHeader.gMC["cond"] &= ~4
+			MyCharHeader.gMC["ani_no"] = 5
+		else:
+			if MyCharHeader.gMC["cond"] & 4:
+				play_sound(24)
+			MyCharHeader.gMC["cond"] &= ~4
+			MyCharHeader.gMC["ani_no"] = 0
+	else:
+		if MyCharHeader.gMC["up"]:
+			MyCharHeader.gMC["ani_no"] = 6
+		elif MyCharHeader.gMC["down"]:
+			MyCharHeader.gMC["ani_no"] = 10
+		else:
+			if MyCharHeader.gMC["ym"] > 0:
+				MyCharHeader.gMC["ani_no"] = 1
+			else:
+				MyCharHeader.gMC["ani_no"] = 3
+
+	# Set the sprite rect depending on the direction
+	if MyCharHeader.gMC["direct"] == 0:
+		MyCharHeader.gMC["rect"] = rc_left[MyCharHeader.gMC["ani_no"]]
+	else:
+		MyCharHeader.gMC["rect"] = rc_right[MyCharHeader.gMC["ani_no"]]
 
 func ShowMyChar(bShow : bool) -> void:
     pass
